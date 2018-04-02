4242a4055c1a553022b75a32c2f19119600b047e
SherlockLiang <lucklzongyu@126.com>
2018-04-02 20:41:17 +0800

    Update .gitignore

--- README.md
+++
@@ -1,3 +0,0 @@
-# TimeManager
-
-This is my practise project. It is for time management on web page.
--- timemanager-manager/timemanager-manager-entity/sql/version1/create/table/create_class_group.sql
+++
@@ -1,7 +0,0 @@
-DROP TABLE class_group IF EXISTS;
-CREATE TABLE class_group {
-	id long identity,
-	name varchar(50),
-	description varchar(100),
-	primary key(id)
-};
